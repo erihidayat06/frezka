7f9e9c9cdd8a1a1d608d5abcd4622c6062eecc27
erihidayat06 <erihidayat549@gmail.com>
2025-06-03 11:38:17 +0700

    Merubah mail

--- config/mail.php
+++ config/mail.php
@@ -114,12 +114,4 @@ return [
         ],
     ],
 
-    'stream' => [
-        'ssl' => [
-            'allow_self_signed' => true,
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-        ],
-    ],
-
 ];
